53c97bb057692026f26fc3fb704ff0993860d254
tejaummineni-4257 <tejaummineni@gmail.com>
2025-11-02 12:35:05 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = ''https://college-event-management-system-dvr3.onrender.com'';
+const API_URL = 'https://college-event-management-system-dvr3.onrender.com/api';
+
 
 export const registerUser = (userData) => axios.post(`${API_URL}/users/register`, userData);
 export const loginUser = (userData) => axios.post(`${API_URL}/users/login`, userData);
