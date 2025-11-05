79ab5565a08cda37a4a14f89c74e023e1eea882f
tejaummineni-4257 <tejaummineni@gmail.com>
2025-11-05 13:35:36 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://college-event-backend.onrender.com/api";
 
 // ✅ Users
 export const registerUser = (userData) =>
